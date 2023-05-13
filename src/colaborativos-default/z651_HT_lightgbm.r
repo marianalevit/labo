60525e87dfc7177b99053968e11618a59de36813
marianalevit <mariana.levit@gmail.com>
2023-05-13 17:02:27 -0300

    new

--- src/colaborativos-default/z651_HT_lightgbm.r
+++ src/colaborativos-default/z651_HT_lightgbm.r
@@ -37,7 +37,7 @@ PARAM$exp_input  <- "TS6410"
 
 PARAM$lgb_crossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
-PARAM$lgb_semilla  <- 102191   #cambiar por su propia semilla
+PARAM$lgb_semilla  <- 379867   #cambiar por su propia semilla
 
 
 #Hiperparametros FIJOS de  lightgbm
