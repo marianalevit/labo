8a30bbefaa86ce06a6376cab5a039524b7c91d82
marianalevit <mariana.levit@gmail.com>
2023-05-20 18:21:23 -0300

    n

--- src/colaborativos-default/z641_TS_training_strategy-kb1.r
+++ src/colaborativos-default/z641_TS_training_strategy-kb1.r
@@ -27,8 +27,8 @@ PARAM$future       <- c( 202109 )
 PARAM$final_train  <- c( 202107, 202106, 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911 )
 
 PARAM$train$training     <- c( 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911, 201910, 201909 )
-PARAM$train$validation   <- c( 202105 )
-PARAM$train$testing      <- c( 202106 )
+PARAM$train$validation   <- c( 202106 )
+PARAM$train$testing      <- c( 202107 )
 
 # Atencion  0.4  de  undersampling de la clase mayoritaria,  los CONTINUA
 PARAM$train$undersampling  <- 0.4   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
