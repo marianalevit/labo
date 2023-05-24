61705c4849bd8ab70b892ab4cf5b4068dfd9741f
marianalevit <mariana.levit@gmail.com>
2023-05-24 19:58:08 -0300

    n

--- src/workflow-semillerio/z731_FE_historia.r
+++ src/workflow-semillerio/z731_FE_historia.r
@@ -54,11 +54,11 @@ PARAM$RandomForest$num.trees  <- 20
 PARAM$RandomForest$max.depth  <-  4
 PARAM$RandomForest$min.node.size  <- 1000
 PARAM$RandomForest$mtry  <- 40
-PARAM$RandomForest$semilla  <- 102191    # cambiar por la propia semilla
+PARAM$RandomForest$semilla  <- 379867    # cambiar por la propia semilla
 
-PARAM$CanaritosAsesinos$ratio  <- 0.0        #varia de 0.0 a 2.0, si es 0.0 NO se activan
-PARAM$CanaritosAsesinos$desvios  <- 4.0      #desvios estandar de la media, para el cutoff
-PARAM$CanaritosAsesinos$semilla  <- 200177   # cambiar por la propia semilla
+PARAM$CanaritosAsesinos$ratio  <- 0.3        #varia de 0.0 a 2.0, si es 0.0 NO se activan
+PARAM$CanaritosAsesinos$desvios  <- 7.0      #desvios estandar de la media, para el cutoff
+PARAM$CanaritosAsesinos$semilla  <- 379867   # cambiar por la propia semilla
 
 PARAM$home  <- "~/buckets/b1/"
 # FIN Parametros del script
